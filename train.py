d22a0990ced016c8ba06d9bc8f30d1fc2682dec6
Ivan Dudakov <call228654@mail.ru>
2022-09-11 23:46:54 +0300

    Update train.py

--- train.py
+++ train.py
@@ -20,10 +20,4 @@ def train(input_dir, model='model.pickle'):
                 data[(sentence[i], sentence[i + 1])] = data.get(sentence[i], []) + [sentence[i + 2]]
     with open(model, 'wb') as file:
         pickle.dump(data, file)
-#my_parser = argparse.ArgumentParser(description='Обучение модели')
-#my_parser.add_argument('directory', metavar='directory', type=str, help='directory with files')
-#my_parser.add_argument('model', type=str, help='file with model')
-#args = my_parser.parse_args()
-# train(args.directory='data', args.model='model.pickle')
 train('data', 'model.pickle')
-print('Обучение завершено')
